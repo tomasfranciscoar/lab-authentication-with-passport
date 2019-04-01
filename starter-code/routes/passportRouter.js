6fa07c43dac4b2a4b5a0ce59de38b635620c5c84
Tomas Freire Knight <tomasfreire@gmail.com>
2019-03-31 22:30:00 -0600

    signup done

--- starter-code/routes/passportRouter.js
+++ starter-code/routes/passportRouter.js
@@ -1,15 +1,52 @@
 const express        = require("express");
 const passportRouter = express.Router();
-// Require user model
+const User = require('../models/User');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
 
-// Add bcrypt to encrypt passwords
+passportRouter.get('/signup', (req, res) => {
+  res.render('passport/signup')
+})
 
-// Add passport 
+passportRouter.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+  .then(user => {
+    if (user !== null) {
+      res.render("auth/signup", { message: "The username already exists" });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  })
+  .catch(error => {
+    next(error)
+  })
+})
 
 const ensureLogin = require("connect-ensure-login");
-
-
 passportRouter.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("passport/private", { user: req.user });
 });
